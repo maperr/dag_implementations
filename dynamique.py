43a1518ef313650a227a6037f061fa7e3c912a31
Maxime Perusse <Maxime Perusse>
2017-03-03 23:23:25 -0500

    init

---
+++ dynamique.py
@@ -0,0 +1,27 @@
+from collections import defaultdict, deque
+import math
+
+# Class to represent a graph
+class Graph:
+    # creates a graph from a file
+    def __init__(self, f):
+        with open(f) as file:
+            lines = file.readlines()
+
+        # get nb of vertices
+        nbVertices = int(lines[0].split(" ")[0])
+        lines.pop(0)
+        self.V = nbVertices  # No. of vertices
+        self.graph = defaultdict(list)  # dictionary containing adjacency List
+        for i in range(0,nbVertices):
+            self.graph[i] = []
+
+        for l in lines:
+            l = l.replace('\n', '')
+            v = int(l.split(" ")[0])
+            e = int(l.split(" ")[1])
+            self.addEdge(v, e)
+
+    # function to add an edge to graph
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
