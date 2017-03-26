e33708e43e28ba3d1e404a5c5e591486ad9d7ff0
Maxime Perusse <Maxime Perusse>
2017-03-26 00:40:42 -0400

    lol

--- dynamique.py
+++ dynamique.py
@@ -1,5 +1,12 @@
 from collections import defaultdict, deque
+import itertools
+import copy
 import datetime
+import sys
+
+def main(argv):
+    print('Number of arguments:', len(sys.argv), 'arguments.')
+    print('Argument List:', str(sys.argv))
 
 # Class to represent a graph
 class Graph:
@@ -10,6 +17,8 @@ class Graph:
 
         self.mat = []
         self.longestChains = []
+        self.matrixIndexVector = []
+        self.iteratorVector = []
 
         self.V = int(lines[0].split(" ")[0])  # No. of vertices
         lines.pop(0)
@@ -76,6 +85,8 @@ class Graph:
             c = self.longestChain()
         for u in self.graph:
             L.append(u)
+        for l in L:
+            l.insert(0,-1)
         self.longestChains = L
 
     def floydFermetureTransitive(self):
@@ -97,32 +108,94 @@ class Graph:
     def createMat(self, L):
         v = []
         for l in L:
-            v.append(len(l) + 1)
+            v.append(len(l))
 
         length = 1
         for i in v:
             length *= i
-        self.mat = [1] * length
+        self.mat = [0] * length
+        self.mat[0] = 1
 
     def dynamic(self, p, t):
-        self.floydFermetureTransitive() # effectuer la fermeture transitive
+        graph = copy.deepcopy(self.graph)
+
+        time_init = datetime.datetime.now()
         self.vorace()
+        self.graph = graph
         self.createMat(self.longestChains)
+        self.floydFermetureTransitive() # effectuer la fermeture transitive
 
-        v = [0] * len(self.longestChains) # represente l'index de l'elem que l'on accede
-        for i in self.mat:
-            i = self.longestChains - 1
-            for c in self.longestChains:
-                v[i]
+        # calculate the matrix index vector needed to get the matrix index from the vector indexes
+        self.matrixIndexVector = [0] * len(self.longestChains)
+        i = len(self.longestChains) - 1
+        m = 1
+        for c in reversed(self.longestChains):
+            self.matrixIndexVector[i] = m
+            m *= len(c)
+            i -= 1
+
+        # iterate through all the elements
+        self.iteratorVector = [0] * len(self.longestChains)
+        for i in range(len(self.longestChains)):
+            n = len(self.longestChains[i])
+            v = []
+            for j in range(n):
+                v.append(j)
+            self.iteratorVector[i] = v
+        for index in itertools.product(*self.iteratorVector):
+            self.calcLinExt(list(index))
+
+        time_end = datetime.datetime.now()
+        time_delta = time_end - time_init
+        if p:
+            print(self.mat[-1])
+        if t:
+            print(time_delta)
+
+    def getMatIndex(self, index):
+        m = 0  # matrix index
+        for c in range(len(self.matrixIndexVector)):
+            m += self.matrixIndexVector[c] * index[c]
+        return m
+
+    def getMatValue(self, index):
+        return self.mat[self.getMatIndex(index)]
+
+    def calcLinExt(self, index):
+        m = self.getMatIndex(index)
+
+        if m == 0: # cas special: s'il s'agit du premier elem, on assigne 1 simplement
+            self.mat[0] = 1
+            return
+
+        result = 0
+        for i in range(len(index)):
+            sub_graph = copy.deepcopy(index)
+            sub_graph[i] -= 1
+            if sub_graph[i] == -1: continue
+            removed_vertex = self.longestChains[i][sub_graph[i]+1]
+            sigma = 1
+            for j in range(len(index)):
+                if j == i: continue
+                end_vertex = self.longestChains[j][sub_graph[j]]
+                if self.graph[removed_vertex].__contains__(end_vertex):
+                    sigma = 0
+            if sigma == 1:
+                result += self.getMatValue(sub_graph)
+        self.mat[m] = result
+
+    def prettyPrint(self, index, tab):
+        s = ""
+        for t in range(tab):
+            s += "   "
+        s += "m["
+        for i in range(len(index)):
+            s += str(self.longestChains[i][index[i]])
+            s += " "
+        s += "]"
+        print(s)
 
 
-    def calcLinExt(self, v):
-        for i in v:
-            print(str(i) + ' ')
-        print('\n')
-
-
-
-#g = Graph("tp2-donnees/poset18-8c")
-g = Graph('ex')
-g.dynamic(True, True)
+#g = Graph("tp2-donnees/poset14-8g")
+#g = Graph('ex')
+#g.dynamic(True, True)
